904d943af2f0aaec4be6a4c2575d0012f9d2462c
Tam Nguyen <tamntrung@gmail.com>
2023-06-14 21:09:37 +0000

    Created mobile responsive layout #1

--- js/data.js
+++ js/data.js
@@ -8,7 +8,7 @@ const data = {
   entryId: 0
 };
 
-const $setWrapper = document.querySelectorAll('.set-wrapper');
+const $setList = document.querySelectorAll('.set-list');
 
 const cardSets = new XMLHttpRequest();
 cardSets.open('GET', 'https://api.pokemontcg.io/v2/sets');
@@ -16,34 +16,43 @@ cardSets.responseType = 'json';
 cardSets.addEventListener('load', function () {
   for (let i = 0; i < cardSets.response.data.length; i++) {
     if (cardSets.response.data[i].series === 'Scarlet & Violet') {
+      const $setWrapper = document.createElement('div');
       const $img = document.createElement('img');
       const $setName = document.createElement('div');
+      $setWrapper.className = 'set-wrapper';
       $img.setAttribute('src', cardSets.response.data[i].images.logo);
       $img.setAttribute('class', 'set');
       $img.setAttribute('data-view-id', cardSets.response.data[i].id);
       $setName.textContent = cardSets.response.data[i].name;
-      $setWrapper[0].appendChild($img);
-      $setWrapper[0].appendChild($setName);
+      $setWrapper.appendChild($img);
+      $setWrapper.appendChild($setName);
+      $setList[0].appendChild($setWrapper);
     }
     if (cardSets.response.data[i].series === 'Sword & Shield') {
+      const $setWrapper = document.createElement('div');
       const $img = document.createElement('img');
       const $setName = document.createElement('div');
+      $setWrapper.className = 'set-wrapper';
       $img.setAttribute('src', cardSets.response.data[i].images.logo);
       $img.setAttribute('class', 'set');
       $img.setAttribute('data-view-id', cardSets.response.data[i].id);
       $setName.textContent = cardSets.response.data[i].name;
-      $setWrapper[1].appendChild($img);
-      $setWrapper[1].appendChild($setName);
+      $setWrapper.appendChild($img);
+      $setWrapper.appendChild($setName);
+      $setList[1].appendChild($setWrapper);
     }
     if (cardSets.response.data[i].series === 'Sun & Moon') {
+      const $setWrapper = document.createElement('div');
       const $img = document.createElement('img');
       const $setName = document.createElement('div');
+      $setWrapper.className = 'set-wrapper';
       $img.setAttribute('src', cardSets.response.data[i].images.logo);
       $img.setAttribute('class', 'set');
       $img.setAttribute('data-view-id', cardSets.response.data[i].id);
       $setName.textContent = cardSets.response.data[i].name;
-      $setWrapper[2].appendChild($img);
-      $setWrapper[2].appendChild($setName);
+      $setWrapper.appendChild($img);
+      $setWrapper.appendChild($setName);
+      $setList[2].appendChild($setWrapper);
     }
   }
 });
--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@ for (let i = 0; i < coll.length; i++) {
   coll[i].addEventListener('click', function () {
     this.classList.toggle('active');
     const content = this.nextElementSibling;
-    if (content.style.display === 'block') {
+    if (content.style.display === 'flex') {
       content.style.display = 'none';
     } else {
-      content.style.display = 'block';
+      content.style.display = 'flex';
     }
   });
 }
