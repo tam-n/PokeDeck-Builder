40fb418c6632366dce8805d687ff6365558a2fc0
Tam Nguyen <tamntrung@gmail.com>
2023-06-16 19:08:20 +0000

    Moved code to main.js file #1

--- js/main.js
+++ js/main.js
@@ -9,6 +9,55 @@ const $deckColumn = document.querySelector('.deck-column');
 const $header = document.querySelector('header');
 const $setLogo = document.querySelector('.set-logo');
 const $cardWrapper = document.querySelector('.card-wrapper');
+const $setList = document.querySelectorAll('.set-list');
+
+const cardSets = new XMLHttpRequest();
+cardSets.open('GET', 'https://api.pokemontcg.io/v2/sets');
+cardSets.responseType = 'json';
+cardSets.addEventListener('load', function () {
+  for (let i = 0; i < cardSets.response.data.length; i++) {
+    if (cardSets.response.data[i].series === 'Scarlet & Violet') {
+      const $setWrapper = document.createElement('div');
+      const $img = document.createElement('img');
+      const $setName = document.createElement('div');
+      $setWrapper.className = 'set-wrapper';
+      $img.setAttribute('src', cardSets.response.data[i].images.logo);
+      $img.setAttribute('class', 'set');
+      $img.setAttribute('data-view-id', cardSets.response.data[i].id);
+      $setName.textContent = cardSets.response.data[i].name;
+      $setWrapper.appendChild($img);
+      $setWrapper.appendChild($setName);
+      $setList[0].appendChild($setWrapper);
+    }
+    if (cardSets.response.data[i].series === 'Sword & Shield') {
+      const $setWrapper = document.createElement('div');
+      const $img = document.createElement('img');
+      const $setName = document.createElement('div');
+      $setWrapper.className = 'set-wrapper';
+      $img.setAttribute('src', cardSets.response.data[i].images.logo);
+      $img.setAttribute('class', 'set');
+      $img.setAttribute('data-view-id', cardSets.response.data[i].id);
+      $setName.textContent = cardSets.response.data[i].name;
+      $setWrapper.appendChild($img);
+      $setWrapper.appendChild($setName);
+      $setList[1].appendChild($setWrapper);
+    }
+    if (cardSets.response.data[i].series === 'Sun & Moon') {
+      const $setWrapper = document.createElement('div');
+      const $img = document.createElement('img');
+      const $setName = document.createElement('div');
+      $setWrapper.className = 'set-wrapper';
+      $img.setAttribute('src', cardSets.response.data[i].images.logo);
+      $img.setAttribute('class', 'set');
+      $img.setAttribute('data-view-id', cardSets.response.data[i].id);
+      $setName.textContent = cardSets.response.data[i].name;
+      $setWrapper.appendChild($img);
+      $setWrapper.appendChild($setName);
+      $setList[2].appendChild($setWrapper);
+    }
+  }
+});
+cardSets.send();
 
 for (let i = 0; i < coll.length; i++) {
   coll[i].addEventListener('click', function () {
@@ -79,16 +128,14 @@ $cardWrapper.addEventListener('click', function (event) {
   }
 });
 
-const $cardSetList = document.querySelectorAll('.set-list');
-
 $header.addEventListener('click', function (event) {
   if (event.target.matches('.home-page')) {
     viewSwap('view-home');
     $myLinks.style.display = 'none';
     clearCardCollection();
     data.cardCollection = [];
-    for (let i = 0; i < $cardSetList.length; i++) {
-      $cardSetList[i].style.display = 'none';
+    for (let i = 0; i < $setList.length; i++) {
+      $setList[i].style.display = 'none';
     }
     $displayCollectionCard.setAttribute('src', 'images/pokemon-card-backside.png');
   }
