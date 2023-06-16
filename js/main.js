b5572cb85bed32325f27daecac416cd81e684c2b
Tam Nguyen <tamntrung@gmail.com>
2023-06-16 18:58:43 +0000

    Fixed view swapping issues with collapsible #1

--- js/main.js
+++ js/main.js
@@ -6,6 +6,9 @@ const $viewHome = document.querySelector('.view-home');
 const $viewSet = document.querySelector('.view-set');
 const $tab = document.querySelectorAll('.tab');
 const $deckColumn = document.querySelector('.deck-column');
+const $header = document.querySelector('header');
+const $setLogo = document.querySelector('.set-logo');
+const $cardWrapper = document.querySelector('.card-wrapper');
 
 for (let i = 0; i < coll.length; i++) {
   coll[i].addEventListener('click', function () {
@@ -41,6 +44,7 @@ function viewSwap(view) {
   if (view === 'view-home') {
     $viewHome.classList.remove('hidden');
     $viewSet.classList.add('hidden');
+    $deckColumn.classList.remove('hidden');
   } else if (view === 'view-set') {
     $viewSet.classList.remove('hidden');
     $viewHome.classList.add('hidden');
@@ -49,9 +53,6 @@ function viewSwap(view) {
   }
 }
 
-const $setLogo = document.querySelector('.set-logo');
-const $cardWrapper = document.querySelector('.card-wrapper');
-
 function renderPokemonCards(setId) {
   const pokemonCards = new XMLHttpRequest();
   pokemonCards.open('GET', 'https://api.pokemontcg.io/v2/cards?orderBy=number&q=set.id:' + setId);
@@ -59,10 +60,11 @@ function renderPokemonCards(setId) {
   pokemonCards.addEventListener('load', function () {
 
     for (let i = 0; i < pokemonCards.response.data.length; i++) {
+      data.cardCollection.push(pokemonCards.response.data[i]);
       const $card = document.createElement('img');
       $card.className = 'card';
-      $card.setAttribute('data-card-id', pokemonCards.response.data[i].id);
-      $card.setAttribute('src', pokemonCards.response.data[i].images.large);
+      $card.setAttribute('data-card-id', data.cardCollection[i].id);
+      $card.setAttribute('src', data.cardCollection[i].images.large);
       $cardWrapper.appendChild($card);
     }
   });
@@ -76,3 +78,22 @@ $cardWrapper.addEventListener('click', function (event) {
     $displayCollectionCard.setAttribute('src', event.target.src);
   }
 });
+
+const $cardSetList = document.querySelectorAll('.set-list');
+
+$header.addEventListener('click', function (event) {
+  if (event.target.matches('.home-page')) {
+    viewSwap('view-home');
+    $myLinks.style.display = 'none';
+    clearCardCollection();
+    data.cardCollection = [];
+    for (let i = 0; i < $cardSetList.length; i++) {
+      $cardSetList[i].style.display = 'none';
+    }
+    $displayCollectionCard.setAttribute('src', 'images/pokemon-card-backside.png');
+  }
+});
+
+function clearCardCollection() {
+  $cardWrapper.innerHTML = '';
+}
